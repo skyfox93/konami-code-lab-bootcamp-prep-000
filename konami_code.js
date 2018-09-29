c3c85fd24f7732c969263a8cbb93a8ae643731ef
Skylar Salerno <skylar.salerno@gmail.com>
2018-09-29 02:50:50 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -7,11 +7,11 @@ const codes = [
   "ArrowRight",
   "ArrowLeft",
   "ArrowRight",
-  "KeyB",
-  "KeyA"
+  "b",
+  "a"
 ];
 var pressed=[];
-function checkCode(e){pressed.push(e.code);var temp=pressed.slice(-10);console.log(temp);for(var i=0;i<10;i++)
+function checkCode(e){pressed.push(e.key);var temp=pressed.slice(-10);console.log(temp);for(var i=0;i<10;i++)
 {if (temp[i]!==codes[i]){return}}
 alert('Congrats!');}
 function init() {
